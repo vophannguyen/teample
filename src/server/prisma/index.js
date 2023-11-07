0452a3e7c7bc195fc5aef6e5680c7afc028d9e85
Nguyen Vo <vophannguyen@gmail.com>
2023-11-07 13:05:54 -0500

    seed fix

--- src/server/prisma/index.js
+++ src/server/prisma/index.js
@@ -3,16 +3,16 @@ const { PrismaClient } = require("@prisma/client");
 let prisma = new PrismaClient();
 
 // Hash user password before saving to database
-prisma = prisma.$extends({
-  query: {
-    user: {
-      async create({ args, query }) {
-        const password = await bcrypt.hash(args.data.password, 10);
-        args.data.password = password;
-        return query(args);
-      },
-    },
-  },
-});
+// prisma = prisma.$extends({
+//   query: {
+//     user: {
+//       async create({ args, query }) {
+//         const password = await bcrypt.hash(args.data.password, 10);
+//         args.data.password = password;
+//         return query(args);
+//       },
+//     },
+//   },
+// });
 
 module.exports = prisma;
