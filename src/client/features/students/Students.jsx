f645fcfb15c5fb3707390a6784e79a387b241ba6
HaruGrossman <dogtaboodog@yahoo.com>
2023-11-08 07:26:42 -0600

    Comments and typo correction in Students.jsx

--- src/client/features/students/Students.jsx
+++ src/client/features/students/Students.jsx
@@ -3,14 +3,15 @@ import { selectToken } from "../auth/authSlice";
 import NewStudent from "./NewStudent";
 import { Link } from "react-router-dom";
 import { useGetStudentsQuery } from "./studentSlice";
+// Need to pull in our seed.js data
 
 import "./Students.less";
 
 const StudentCard = ({ student }) => {
   return (
     <li className="student-card">
-      <h2>{student.firstname}</h2>
-      <Link to={`students/${student.id}`} className="details-btn">{student.firstname} details</Link>
+      <h2>{student.firstName}</h2>
+      <Link to={`students/${student.id}`} className="details-btn">{student.firstName} details</Link>
     </li>
   )
 };
@@ -23,15 +24,15 @@ export default function Students() {
   return (
     <div className="students">
       <section className="roster">
-      <h1>Students</h1>
-      {isLoading && <p>Loading student roster...</p>}
-      {students && (
-        <ul>
-          {students.map((student) => (
-            <StudentCard key={student.id} task={student} />
-          ))}
-        </ul>
-      )}
+        <h1>Students</h1>
+        {isLoading && <p>Loading student roster...</p>}
+        {students && (
+          <ul>
+            {students.map((student) => (
+              <StudentCard key={student.id} task={student} /> //Is this the like to single student detail card?
+            ))}
+          </ul>
+        )}
       </section>
       <aside className="add-form">
         <h2>Add New Student</h2>
