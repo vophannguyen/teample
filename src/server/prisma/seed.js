3522179a62cab1b4eda9732891412253cbcdb535
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-11-08 09:47:41 -0600

    fixed courtney's seeding issues

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -1,53 +1,78 @@
 const prisma = require("../prisma");
 
 /** Seeds the database with a user and some tasks */
-const seed = async () => {
-  await prisma.student.create({
-    data: {
-      firstName: "Nguyen",
-      lastName: "Vo",
-      email: "vophannguyen@gmail.com",
-      imageUrl:
-        "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
-      gpa: 3.9,
-    },
-  });
-  await prisma.student.create({
-    data: {
-      firstName: "Anna",
-      lastName: "WaterHouse",
-      email: "anna@gmail.com",
-      imageUrl:
-        "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
-      gpa: 3.9,
-    },
-  });
-  await prisma.student.create({
-    data: {
-      firstName: "Courtney",
-      lastName: "Snyder",
-      email: "court@gmail.com",
-      imageUrl:
-        "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
-      gpa: 3.9,
-    },
-  });
-  await prisma.student.create({
-    data: {
-      firstName: "Haru",
-      lastName: "Grossman",
-      email: "haru@gmail.com",
-      imageUrl:
-        "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
-      gpa: 3.9,
-    },
-  });
+// const seed = async () => {
+//   await prisma.student.create({
+//     data: {
+//       firstName: "Nguyen",
+//       lastName: "Vo",
+//       email: "vophannguyen@gmail.com",
+//       imageUrl:
+//         "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+//       gpa: 3.9,
+//     },
+//   });
+//   await prisma.student.create({
+//     data: {
+//       firstName: "Anna",
+//       lastName: "WaterHouse",
+//       email: "anna@gmail.com",
+//       imageUrl:
+//         "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+//       gpa: 3.9,
+//     },
+//   });
+//   await prisma.student.create({
+//     data: {
+//       firstName: "Courtney",
+//       lastName: "Snyder",
+//       email: "court@gmail.com",
+//       imageUrl:
+//         "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+//       gpa: 3.9,
+//     },
+//   });
+//   await prisma.student.create({
+//     data: {
+//       firstName: "Haru",
+//       lastName: "Grossman",
+//       email: "haru@gmail.com",
+//       imageUrl:
+//         "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+//       gpa: 3.9,
+//     },
+//   });
+// };
+
+// seed()
+//   .then(async () => await prisma.$disconnect())
+//   .catch(async (err) => {
+//     console.error(err);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
+
+
+
+const seed = async (numStudents = 10) => {
+  for (let i = 0; i < numStudents; i++) {
+    await prisma.student.create({
+      data:{
+        firstName: `firstName${i}`,
+        lastName: `lastName${i}`, 
+        email: `${i}@gmail.com`,
+        imageUrl: `https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg`,
+        // later make gpa random
+        gpa: 3.9,
+      },
+    });
+  };
 };
 
 seed()
   .then(async () => await prisma.$disconnect())
-  .catch(async (err) => {
-    console.error(err);
-    await prisma.$disconnect();
+  .catch(async (e) => {
+    console.error(e);
+    await primsa.$disconnect();
     process.exit(1);
-  });
+  })
