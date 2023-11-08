810b9516141c70466e225e7362b62b4e837cc20a
annawaterhouse <annawaterhouse05@gmail.com>
2023-11-08 15:26:07 -0500

    edit form rendering

--- src/client/features/students/StudentDetails.jsx
+++ src/client/features/students/StudentDetails.jsx
@@ -6,19 +6,56 @@ import { useState } from "react";
 
 /** Allows user to read, update, and delete a task */
 export default function StudentDetails() {
-  const [ useDelete ] = useDeleteStudentMutation();
-  const [ useUpdate ] = useUpdateStudentMutation();
+  const [ deleteStudent ] = useDeleteStudentMutation();
+  const [ isValid, setIsValid ] = useState(false);
+  const [ edit, setEdit ] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
+  const [ updateStudent ] = useUpdateStudentMutation(id);
   const { data } = useGetStudentQuery(id);
- 
-  const onEdit = async (e) => {
+  
+  
+  const validateEmail = (e) => {
+    const emailRegex =
+      /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-0]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i;
+    const email = e.target.value;
+    setIsValid(emailRegex.test(email));
+  };
+
+  const onSubmit = async (e) => {
     e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const editStudent = {
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+      email: formData.get("email"),
+      imageUrl: formData.get("imageUrl") || "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+      gpa: formData.get("gpa") || 0.00,
+    };
+
+    try {
+      if (!isValid) {
+        setMessage("Required: valid email address")
+      } else {
+        await updateStudent( id, editStudent ).unwrap();
+        e.target.reset();
+        navigate(`/students/${id}`);
+        setEdit(false);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onEdit = (e) => {
+    e.preventDefault();
+    setEdit(true);
   };
 
   const onDelete = async (id) => {
     try {
-      await useDelete(id).unwrap();
+      await deleteStudent(id).unwrap();
       navigate("/");
     } catch(err) {
       console.log(err);
@@ -29,19 +66,53 @@ export default function StudentDetails() {
     navigate("/");
   };
 
-  return data ? (
-    <section className="student-details">
-      <article className="col-left">
-        <img src={data.imageUrl} alt={data.firstName} />
-        <h2>{data.firstName} {data.lastName}</h2>
-        <h3>GPA: {data.gpa}</h3>
-        <h3>Contact: {data.email}</h3>
-      </article>
-      <aside>
-        <button className="edit-btn" onClick={() => onEdit(data.id)}>Edit Profile</button>
-        <button className="delete-btn" onClick={() => onDelete(data.id)}>Delete</button>
-        <button className="return-btn" onClick={onNavigate}>Return</button>
-      </aside>
-    </section>
-  ) : (<p>Loading...</p>)
+  const editForm = (
+    <form className="edit-form" type="text" onSubmit={onSubmit}>
+    <input name="firstName" type="text" placeholder="Firstname" required />
+    <input name="lastName" type="text" placeholder="Lastname" required />
+    <input
+      name="email"
+      type="text"
+      placeholder="Email"
+      onChange={validateEmail}
+      required
+    />
+    <input name="imageUrl" type="text" placeholder="Image URL" />
+    <input
+      name="gpa"
+      type="number"
+      inputMode="decimal"
+      min={0.0}
+      max={4.0}
+      step="0.01"
+      placeholder="GPA"
+    />
+    <button className="add-btn" type="submit">
+      Save
+    </button>
+    {/* {message && <p>{message}</p>} */}
+  </form>
+  );
+  
+  console.log(data);
+  return (
+    <>
+    { data ? (
+        <section className="student-details">
+          <article className="col-left">
+            <img src={data.imageUrl} alt={data.firstName} />
+            <h2>{data.firstName} {data.lastName}</h2>
+            <h3>GPA: {data.gpa}</h3>
+            <h3>Contact: {data.email}</h3>
+          </article>
+          <aside>
+            <button className="edit-btn" onClick={onEdit}>Edit</button>
+            <button className="delete-btn" onClick={() => onDelete(data.id)}>Delete</button>
+            <button className="return-btn" onClick={onNavigate}>Return</button>
+          </aside>
+        </section>
+    ) : (<p>Loading...</p>) }
+    { edit && editForm }
+    </>
+  )
 }
