e6e9eca9fb2c512ed5d4f032123d23b80a801a9c
HaruGrossman <dogtaboodog@yahoo.com>
2023-11-08 12:50:17 -0600

    Rearranged Card presentation

--- src/client/features/students/Student.jsx
+++ src/client/features/students/Student.jsx
@@ -1,26 +1,30 @@
 //This file is just for viewing a single selected Student, with all their details
-import { useState } from "react";
-import { useDeleteTaskMutation, useEditTaskMutation } from "./studentSlice"
-// import seed from "../seed.js"
-
+import { Link } from "react-router-dom";
+import { useGetSingleStudentQuery } from "./studentSlice";
 
 // Display to user single student full, name, email, image, and gpa 
-function studentDetails({ student }) {
-  // Display the appropriate student when the url matches "/students/:studentId"
+const StudentDetailCard = ({ student }) => {
   return (
     <>
-      <div>`Student ${studentFirstName} ${studentLastName}`</div>
-      <br />
-      <div>`Contact Email ${studentEmail}`</div>
-      <br />
-      <image />
-      <br />
-      <div>`GPA ${studentGPA}`</div>
-      {/* Display name of their campus (or helpful message if they don't have one) 
+      <li className="student-details-card">
+        <div>{`Student ${student.id.studentFirstName} ${student.id.studentLastName}`}</div>
+        <br />
+        <div>{`Contact Email ${student.id.studentEmail}`}</div>
+        <br />
+        <image />
+        <br />
+        <div>{`GPA ${student.id.studentGPA}`}</div>
+        {/* Display name of their campus (or helpful message if they don't have one) 
       if campus */}
-      <div>{studentCampus ? studentCampus : `Campus not listed`}</div>
+        <div>{student.id.studentCampus ? student.id.studentCampus : `Campus not listed`}</div>
+      </li>
     </>
   )
+}
+
+function studentDetails({ student }) {
+  // Display the appropriate student when the url matches "/students/:studentId"
+  return "is working?"
 }
 export default studentDetails
 
