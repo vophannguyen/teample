2b1fc42c6d6c5ca863e303f631a7d07e0613eefd
HaruGrossman <dogtaboodog@yahoo.com>
2023-11-08 12:50:49 -0600

    Added slice and not just api

--- src/client/features/students/studentSlice.js
+++ src/client/features/students/studentSlice.js
@@ -18,3 +18,24 @@ const studentsApi = api.injectEndpoints({
 export const {
   useGetStudentsQuery, useGetSingleStudentQuery
 } = studentsApi;
+
+
+//? should this file be for our slices and not tha api?
+import { createSlice } from "@reduxjs/toolkit"
+
+const studentSlice = createSlice({
+  name: "students",
+  initialState: [],
+  reducers: {
+    addStudent: (state, { payload }) => {
+      return state.concat([{ student: payload, done: false }])
+    }
+    viewSingleStudent: (state, { payload }) => {
+      return state.concat([{ student: payload }])
+    }
+  }
+})
+
+export const { addStudent } = studentSlice.actions
+
+export default studentSlice.reducer
