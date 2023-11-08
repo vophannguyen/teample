efa9d4563f28bc2c7f23107ac5c2a6c65985b2c7
Nguyen Vo <vophannguyen@gmail.com>
2023-11-08 18:16:50 -0500

    fix update

--- src/client/features/students/StudentDetails.jsx
+++ src/client/features/students/StudentDetails.jsx
@@ -1,21 +1,23 @@
-
 //just testing
-import { useGetStudentQuery, useDeleteStudentMutation, useUpdateStudentMutation } from "./studentSlice"
+import {
+  useGetStudentQuery,
+  useDeleteStudentMutation,
+  useUpdateStudentMutation,
+} from "./studentSlice";
 import { useParams, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 /** Allows user to read, update, and delete a task */
 export default function StudentDetails() {
-  const [ deleteStudent ] = useDeleteStudentMutation();
-  const [ message, setMessage ] = useState(null);
-  const [ isValid, setIsValid ] = useState(false);
-  const [ edit, setEdit ] = useState(false);
+  const [deleteStudent] = useDeleteStudentMutation();
+  const [message, setMessage] = useState(null);
+  const [isValid, setIsValid] = useState(false);
+  const [edit, setEdit] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
-  const [ updateStudent ] = useUpdateStudentMutation();
+  const [updateStudent] = useUpdateStudentMutation();
   const { data } = useGetStudentQuery(id);
-  
-  
+
   const validateEmail = (e) => {
     const emailRegex =
       /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-0]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i;
@@ -31,18 +33,21 @@ export default function StudentDetails() {
       firstName: formData.get("firstName"),
       lastName: formData.get("lastName"),
       email: formData.get("email"),
-      imageUrl: formData.get("imageUrl") || "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
-      gpa: formData.get("gpa") || 0.00,
+      imageUrl:
+        formData.get("imageUrl") ||
+        "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+      gpa: formData.get("gpa") || 0.0,
     };
 
     try {
       if (!isValid) {
-        setMessage("Required: valid email address")
+        setMessage("Required: valid email address");
       } else {
-        await updateStudent( id, editStudent ).unwrap();
+        console.log(editStudent);
+        const response = await updateStudent({ id, ...editStudent });
+        console.log(response);
         e.target.reset();
         setEdit(false);
-        console.log(editStudent);
         navigate(`/students/${id}`);
       }
     } catch (err) {
@@ -59,7 +64,7 @@ export default function StudentDetails() {
     try {
       await deleteStudent(id).unwrap();
       navigate("/");
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
   };
@@ -70,50 +75,60 @@ export default function StudentDetails() {
 
   const editForm = (
     <form className="edit-form" type="text" onSubmit={onSubmit}>
-    <input name="firstName" type="text" placeholder="Firstname" required />
-    <input name="lastName" type="text" placeholder="Lastname" required />
-    <input
-      name="email"
-      type="text"
-      placeholder="Email"
-      onChange={validateEmail}
-      required
-    />
-    <input name="imageUrl" type="text" placeholder="Image URL" />
-    <input
-      name="gpa"
-      type="number"
-      inputMode="decimal"
-      min={0.0}
-      max={4.0}
-      step="0.01"
-      placeholder="GPA"
-    />
-    <button className="add-btn" type="submit">
-      Save
-    </button>
-    {message && <p>{message}</p>}
-  </form>
+      <input name="firstName" type="text" placeholder="Firstname" required />
+      <input name="lastName" type="text" placeholder="Lastname" required />
+      <input
+        name="email"
+        type="text"
+        placeholder="Email"
+        onChange={validateEmail}
+        required
+      />
+      <input name="imageUrl" type="text" placeholder="Image URL" />
+      <input
+        name="gpa"
+        type="number"
+        inputMode="decimal"
+        min={0.0}
+        max={4.0}
+        step="0.01"
+        placeholder="GPA"
+      />
+      <button className="add-btn" type="submit">
+        Save
+      </button>
+      {message && <p>{message}</p>}
+    </form>
   );
-  
+
   return (
     <>
-    { data ? (
+      {data ? (
         <section className="student-details">
           <article className="col-left">
             <img src={data.imageUrl} alt={data.firstName} />
-            <h2>{data.firstName} {data.lastName}</h2>
+            <h2>
+              {data.firstName} {data.lastName}
+            </h2>
             <h3>GPA: {data.gpa}</h3>
             <h3>Contact: {data.email}</h3>
           </article>
           <aside>
-            <button className="edit-btn" onClick={onEdit}>Edit</button>
-            <button className="delete-btn" onClick={() => onDelete(data.id)}>Delete</button>
-            <button className="return-btn" onClick={onNavigate}>Return</button>
+            <button className="edit-btn" onClick={onEdit}>
+              Edit
+            </button>
+            <button className="delete-btn" onClick={() => onDelete(data.id)}>
+              Delete
+            </button>
+            <button className="return-btn" onClick={onNavigate}>
+              Return
+            </button>
           </aside>
         </section>
-    ) : (<p>Loading...</p>) }
-    { edit && editForm }
+      ) : (
+        <p>Loading...</p>
+      )}
+      {edit && editForm}
     </>
-  )
+  );
 }
--- src/client/store/api.js
+++ src/client/store/api.js
@@ -8,11 +8,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:8080/api",
+
     // prepareHeaders: (headers, { getState }) => {
-    //   const token = getState().auth.token;
-    //   if (token) {
-    //     headers.set("authorization", `Bearer ${token}`);
-    //   }
+    //   headers.set("Content-Type", "multipart/form-data");
     //   return headers;
     // },
   }),
